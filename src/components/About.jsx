fed351cbbb144e1b2f681b3eadcc719f5f17f6c2
vijaykalashikam <vijaykalashikam@gmail.com>
2024-08-30 11:10:21 +0530

    main changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const About =() =>{
   return (
     <div>
-        <h1>
-  
-        </h1>
+      about_us
     </div>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import  { Outlet } from "react-router-dom"
-import Home from './Home';
- const Layout=()=> {
-  return (
-    <>
-    <div>
-      <Home/>
 
-        {/* <Link to="/about_us">About</Link>
-        <Link to="/homepage">Home</Link> */}
-        <Outlet/>
+const Layout = () => {
+  return (
+    <div>
+      Layout
     </div>
-    </>
   )
 }
 
